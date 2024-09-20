7d681ae98ff21e92083a6f825ee67907b1c1e955
Darshan M.S. <udaydarshan@gmail.com>
2024-09-20 06:43:17 +0000

    make stg_bingeflix__events incremental

--- models/staging/bingeflix/stg_bingeflix__events.sql
+++ models/staging/bingeflix/stg_bingeflix__events.sql
@@ -1,8 +1,20 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='event_id'
+    )
+}}
+
 with source as (
 
     select * from {{ source('bingeflix', 'events') }}
 
+    {% if is_incremental() %}
+    where created_at > (select dateadd('day', -3, max(created_at)) from {{ this }})
+    {% endif %}
+
 ),
+
 
 renamed as (
 
